96a4995455c4a3ffab15c2ce350386fc4ada8a01
jarray-thamer <thamer99jarray@gmail.com>
2025-09-18 07:24:08 +0100

    changed params type

--- app/api/events/[slug]/route.ts
+++ app/api/events/[slug]/route.ts
@@ -1,11 +1,12 @@
+import { NextRequest } from "next/server";
 import { prisma } from "@/lib/db";
 
 export async function GET(
-  _req: Request,
+  _req: NextRequest,
   { params }: { params: { slug: string } }
 ) {
-  const { slug } = await params;
   try {
+    const { slug } = await params; // Await the params promise
     const event = await prisma.event.findUnique({
       where: { slug },
       select: {
@@ -18,11 +19,13 @@ export async function GET(
         createdAt: true,
       },
     });
+
     if (!event) {
       return new Response(JSON.stringify({ error: "Not found" }), {
         status: 404,
       });
     }
+
     return Response.json({ event });
   } catch (e) {
     return new Response(JSON.stringify({ error: "Failed to fetch" }), {
