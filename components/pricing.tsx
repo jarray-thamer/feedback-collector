9a7aad43ddea1b1e5f737bd78131f2482ba96c8c
jarray-thamer <thamer99jarray@gmail.com>
2025-09-18 07:31:24 +0100

    fixing pricing format

--- components/pricing.tsx
+++ components/pricing.tsx
@@ -155,7 +155,6 @@ export function Pricing({
                       minimumFractionDigits: 0,
                       maximumFractionDigits: 0,
                     }}
-                    formatter={(value) => `$${value}`}
                     transformTiming={{
                       duration: 500,
                       easing: "ease-out",
