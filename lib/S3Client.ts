1c1f4f1ba7975a1de24ed4df06d818dcf9dbd1f6
jarray-thamer <thamer99jarray@gmail.com>
2025-09-18 09:54:39 +0100

    added if state for env

--- lib/S3Client.ts
+++ lib/S3Client.ts
@@ -2,8 +2,8 @@ import "server-only";
 
 import { S3Client } from "@aws-sdk/client-s3";
 export const S3 = new S3Client({
-  region: process.env.AWS_REGION,
-  endpoint: process.env.AWS_ENDPOINT_URL_S3,
+  region: process.env.AWS_REGION || process.env.S3_REGION,
+  endpoint: process.env.AWS_ENDPOINT_URL_S3 || process.env.S3_ENDPOINT_URL_S3,
   forcePathStyle: false,
   // credentials: {
   //   accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
