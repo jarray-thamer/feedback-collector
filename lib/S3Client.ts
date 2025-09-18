87c82913e5f3cb486b4d5f30393229e58e03f6e5
jarray-thamer <thamer99jarray@gmail.com>
2025-09-18 07:06:45 +0100

    changed env aws to s3

--- lib/S3Client.ts
+++ lib/S3Client.ts
@@ -2,7 +2,7 @@ import "server-only";
 
 import { S3Client } from "@aws-sdk/client-s3";
 export const S3 = new S3Client({
-  region: process.env.AWS_REGION,
-  endpoint: process.env.AWS_ENDPOINT,
+  region: process.env.S3_REGION,
+  endpoint: process.env.S3_ENDPOINT,
   forcePathStyle: false,
 });
